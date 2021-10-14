eefc31f021f59ea74971361077418141932ad1cb
MuhammadNurFauzi1205 <72461742+MuhammadNurFauzi1205@users.noreply.github.com>
2021-10-14 21:02:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.bluetooth;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.bluetooth.R;
+
+/**
+ * Created by PEACE on 3/30/2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(700);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(MainActivity.this, DeviceList.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
